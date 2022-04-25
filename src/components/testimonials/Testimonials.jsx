1f266e65576bf05f072362cab9312e483313b716
marcos <marcoscorralescastro.daw2020@riberadeltajo.es>
2022-04-25 19:20:58 +0200

    Metodo portfolio y falta carousel testimonials

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,9 +1,59 @@
 import React from 'react'
 import './testimonials.css'
+import AVTR1 from '../../assets/avatar1.jpg'
+import AVTR2 from '../../assets/avatar2.jpg'
+import AVTR3 from '../../assets/avatar3.jpg'
+import AVTR4 from '../../assets/avatar4.jpg'
+
+const data = [
+  {
+    avatar: AVTR1,
+    name: "Ernest Achiever",
+    review:
+      " Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit quas cum animi at iusto, ex quod iste molestias quisquam, fugiat amet magnam distinctio perferendis soluta!",
+  },
+  {
+    avatar: AVTR2,
+    name: "Ernest Achiever",
+    review:
+      " Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit quas cum animi at iusto, ex quod iste molestias quisquam, fugiat amet magnam distinctio perferendis soluta!",
+  },
+  {
+    avatar: AVTR3,
+    name: "Ernest Achiever",
+    review:
+      " Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit quas cum animi at iusto, ex quod iste molestias quisquam, fugiat amet magnam distinctio perferendis soluta!",
+  },
+  {
+    avatar: AVTR4,
+    name: "Ernest Achiever",
+    review:
+      " Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit quas cum animi at iusto, ex quod iste molestias quisquam, fugiat amet magnam distinctio perferendis soluta!",
+  }
+];
 
 const Testimonials = () => {
   return (
-    <section id='testimonials'>Testimonials</section>
+    <section id='testimonials'>
+      <h5>Review from clients</h5>
+      <h2>Testimonials</h2>
+
+      <div className="container testimonials__container">
+      {
+        data.map(({avatar, name, review}, index)=>{
+          return(
+            <article key={index} className="testimonial">
+            <div className="client__avatar">
+              <img src={avatar}/>
+            </div>
+            <h5 className='client__name'>{name}</h5>
+              <small className='client__review'>{review}</small>
+          </article>
+          )
+        })
+      }
+      </div>
+    </section>
   )
 }
 
