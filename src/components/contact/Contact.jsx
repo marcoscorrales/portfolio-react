13d36f9cbcbcbddd726dca3181a11d686bb0b92d
marcos <marcoscorralescastro.daw2020@riberadeltajo.es>
2022-04-28 20:36:45 +0200

    Emailjs y Componente footer

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -4,8 +4,19 @@ import './contact.css'
 import {MdOutlineEmail} from 'react-icons/md'
 import {RiMessengerLine} from 'react-icons/ri'
 import {BsWhatsapp} from 'react-icons/bs'
+import { useRef } from 'react';
+import emailjs from 'emailjs-com';
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_0vzrk7a', 'template_hq99rtn', form.current, 'zrsqD7fEJCy5oEnfO')
+      
+    e.target.reset()
+  };
   return (
     <section id='contact'>
       <h5>Get In Touch</h5>
@@ -33,9 +44,9 @@ const Contact = () => {
           </article>
         </div>
         {/* END OF CONTACT OPTIONS*/}
-        <form action="">
+        <form ref={form} onSubmit={sendEmail}>
           <input type="text" name="name" placeholder='Your Full Name' required/>
-          <input type="email" name="email" placeholder='Your Message' required/>
+          <input type="email" name="email" placeholder='Your Email' required/>
           <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
           <button type="submit" className='btn btn-primary'>Send Message</button>
         </form>
